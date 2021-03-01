f22872219a07ec0b49aa845189ce6059bd1e41e9
liu <liukecyl@163.com>
2021-03-01 10:26:32 +0800

    方法调整

--- ASShareTool/Classes/ASShareTool.h
+++ ASShareTool/Classes/ASShareTool.h
@@ -6,7 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
-//#import <UShareUI/UShareUI.h>
+#import <UShareUI/UShareUI.h>
 NS_ASSUME_NONNULL_BEGIN
 /**
  * 回调
@@ -17,29 +17,29 @@ NS_ASSUME_NONNULL_BEGIN
 typedef void (^ASShareCompletionHandler)(id result,NSError *error);
 
 @interface ASShareTool : NSObject
-///**
-// *  设置分享平台
-// *  @param platform
-// *  @param  thumImage 缩略图（UIImage或者NSData类型，或者image_url）
-// *  @param context  分享的content
-// *  @param contextUrl 用于分享的连接
-// */
-//
-//+(void)sharePreDefinePlatforms:(NSArray *)platforms WithContextImgUrl:(id)thumImage
-//                      context:(NSString *)context
-//                   contextUrl:(NSString *)contextUrl
-//        currentViewController:(UIViewController*)vc
-//                    withTitle:(NSString*)title
-//                   completion:(ASShareCompletionHandler)completion;;
-//
-///**
-// * 自动以分享UI 点击事件可调用次方法
-// *
-// */
-//+ (void)sharePlatform:(UMSocialPlatformType)platformType
-//          messageObject:(UMSocialMessageObject *)messageObject
-//  currentViewController:(id)currentViewController  completion:(ASShareCompletionHandler)completion;
-//
+/**
+ *  设置分享平台
+ *  @param platform
+ *  @param  thumImage 缩略图（UIImage或者NSData类型，或者image_url）
+ *  @param context  分享的content
+ *  @param contextUrl 用于分享的连接
+ */
+
++(void)sharePreDefinePlatforms:(NSArray *)platforms WithContextImgUrl:(id)thumImage
+                      context:(NSString *)context
+                   contextUrl:(NSString *)contextUrl
+        currentViewController:(UIViewController*)vc
+                    withTitle:(NSString*)title
+                   completion:(ASShareCompletionHandler)completion;;
+
+/**
+ * 自动以分享UI 点击事件可调用次方法
+ *
+ */
++ (void)sharePlatform:(UMSocialPlatformType)platformType
+          messageObject:(UMSocialMessageObject *)messageObject
+  currentViewController:(id)currentViewController  completion:(ASShareCompletionHandler)completion;
+
 
 @end
 
--- ASShareTool/Classes/ASShareTool.m
+++ ASShareTool/Classes/ASShareTool.m
@@ -9,66 +9,66 @@
 
 @implementation ASShareTool
 
-//+(void)sharePreDefinePlatforms:(NSArray *)platforms WithContextImgUrl:(id)thumImage context:(NSString *)context contextUrl:(NSString *)contextUrl currentViewController:(UIViewController *)vc withTitle:(NSString *)title completion:(ASShareCompletionHandler)completion{
-//    
-//    [UMSocialUIManager setPreDefinePlatforms:platforms];
-//    [UMSocialUIManager showShareMenuViewInWindowWithPlatformSelectionBlock:^(UMSocialPlatformType platformType, NSDictionary *userInfo) {
-//        NSString *descr;
-//        if(platformType == UMSocialPlatformType_Sms){
-//            descr = [NSString stringWithFormat:@"%@请点击%@查看",context,contextUrl];
-//        }else {
-//            descr = context;
-//        }
-//        UMSocialMessageObject *messageObject = [UMSocialMessageObject messageObject];
-//        //创建网页内容对象
-//        UMShareWebpageObject *shareObject = [UMShareWebpageObject shareObjectWithTitle:title descr:descr thumImage:thumImage];
-//        //设置网页地址
-//        if (platformType == UMSocialPlatformType_Sms) {
-//            shareObject.webpageUrl = @"";
-//        }else{
-//            shareObject.webpageUrl = contextUrl;
-//        }
-//        //分享消息对象设置分享内容对象
-//        messageObject.shareObject = shareObject;
-//        messageObject.text = descr;
-//        
-//        [self sharePlatform:platformType messageObject:messageObject currentViewController:vc completion:completion];
-//    }];
-//    
-//   
-//}
-//
-//
-//+ (void)sharePlatform:(UMSocialPlatformType)platformType messageObject:(UMSocialMessageObject *)messageObject currentViewController:(id)currentViewController completion:(ASShareCompletionHandler)completion{
-//    
-//    [[UMSocialManager defaultManager] shareToPlatform:platformType messageObject:messageObject currentViewController:currentViewController completion:^(id data, NSError *error) {
-//        if (error) {
-//            NSLog(@"************Share fail with error %@*********",error);
-//            NSString *result;
-//            if (error) {
-//                 result = [NSString stringWithFormat:@"%@",error.userInfo[@"message"]];
-//            }
-//            else{
-//                result = [NSString stringWithFormat:@"分享失败"];
-//            }
-//            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"分享" message:result delegate:nil
-//            cancelButtonTitle:@"确定" otherButtonTitles:nil];
-//            [alert show];
-//        }else{
-//            if ([data isKindOfClass:[UMSocialShareResponse class]]) {
-//                UMSocialShareResponse *resp = data;
-//                //分享结果消息
-//                NSLog(@"response message is %@",resp.message);
-//                //第三方原始返回的数据
-//                NSLog(@"response originalResponse data is %@",resp.originalResponse);
-//            }else{
-//                NSLog(@"response data is %@",data);
-//            }
-//        }
-//        completion(data,error);
-//    }];
-//    
-//}
++(void)sharePreDefinePlatforms:(NSArray *)platforms WithContextImgUrl:(id)thumImage context:(NSString *)context contextUrl:(NSString *)contextUrl currentViewController:(UIViewController *)vc withTitle:(NSString *)title completion:(ASShareCompletionHandler)completion{
+    
+    [UMSocialUIManager setPreDefinePlatforms:platforms];
+    [UMSocialUIManager showShareMenuViewInWindowWithPlatformSelectionBlock:^(UMSocialPlatformType platformType, NSDictionary *userInfo) {
+        NSString *descr;
+        if(platformType == UMSocialPlatformType_Sms){
+            descr = [NSString stringWithFormat:@"%@请点击%@查看",context,contextUrl];
+        }else {
+            descr = context;
+        }
+        UMSocialMessageObject *messageObject = [UMSocialMessageObject messageObject];
+        //创建网页内容对象
+        UMShareWebpageObject *shareObject = [UMShareWebpageObject shareObjectWithTitle:title descr:descr thumImage:thumImage];
+        //设置网页地址
+        if (platformType == UMSocialPlatformType_Sms) {
+            shareObject.webpageUrl = @"";
+        }else{
+            shareObject.webpageUrl = contextUrl;
+        }
+        //分享消息对象设置分享内容对象
+        messageObject.shareObject = shareObject;
+        messageObject.text = descr;
+        
+        [self sharePlatform:platformType messageObject:messageObject currentViewController:vc completion:completion];
+    }];
+    
+   
+}
+
+
++ (void)sharePlatform:(UMSocialPlatformType)platformType messageObject:(UMSocialMessageObject *)messageObject currentViewController:(id)currentViewController completion:(ASShareCompletionHandler)completion{
+    
+    [[UMSocialManager defaultManager] shareToPlatform:platformType messageObject:messageObject currentViewController:currentViewController completion:^(id data, NSError *error) {
+        if (error) {
+            NSLog(@"************Share fail with error %@*********",error);
+            NSString *result;
+            if (error) {
+                 result = [NSString stringWithFormat:@"%@",error.userInfo[@"message"]];
+            }
+            else{
+                result = [NSString stringWithFormat:@"分享失败"];
+            }
+            UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"分享" message:result delegate:nil
+            cancelButtonTitle:@"确定" otherButtonTitles:nil];
+            [alert show];
+        }else{
+            if ([data isKindOfClass:[UMSocialShareResponse class]]) {
+                UMSocialShareResponse *resp = data;
+                //分享结果消息
+                NSLog(@"response message is %@",resp.message);
+                //第三方原始返回的数据
+                NSLog(@"response originalResponse data is %@",resp.originalResponse);
+            }else{
+                NSLog(@"response data is %@",data);
+            }
+        }
+        completion(data,error);
+    }];
+    
+}
 
 
 @end
